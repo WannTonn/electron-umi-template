a353fa826ca843352d8db5978c19125d453e50ea
WannTonn <wanntonn428@gmail.com>
2022-09-21 22:12:07 +0800

    feat:更新依赖

--- renderer/src/pages/index.tsx
+++ renderer/src/pages/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function IndexPage() {
-
+  useEffect(() => {
+    // window.location.href = "https://zhidao.baidu.com/pages/consult/index/login"
+  }, [])
   return (
     <div>
       <h1>Page index</h1>
+      {/* <iframe src="https://zhidao.baidu.com/pages/consult/index/login" frameBorder="0"></iframe> */}
+      <webview src="https://zhidao.baidu.com/pages/consult/index/login" style={{width: 1000, height: 500}}></webview>
     </div>
   );
 }
